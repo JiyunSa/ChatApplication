f243ac25a12441f0b747ef080e40c812b7283f82
JiyunSa <81289580+JiyunSa@users.noreply.github.com>
2021-04-17 16:13:26 +0900

    Add files via upload

---
+++ SP-CW2-ChatApplication-1/src/main/java/com/sp/cw2/SpCw2ChatApplication1Application.java
@@ -0,0 +1,13 @@
+package com.sp.cw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpCw2ChatApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpCw2ChatApplication1Application.class, args);
+	}
+
+}
---
+++ SP-CW2-ChatApplication-1/src/test/java/com/sp/cw2/SpCw2ChatApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.cw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpCw2ChatApplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
